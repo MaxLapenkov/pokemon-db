1934b6b18585c15eb9a5350f48425f8625a9bfc6
Maxel <gigaabait@yandex.ru>
2020-04-17 21:07:07 +0400

    Updates

---
+++ precache-manifest.2ef26d5e608f5564e9b76bc25bfe2324.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff96fbc4b49945bcd3cd5f1e95f72585",
+    "url": "/pokemon-db/index.html"
+  },
+  {
+    "revision": "1e704b2696a133b39891",
+    "url": "/pokemon-db/static/css/main.66537e4c.chunk.css"
+  },
+  {
+    "revision": "49e65e02cba3978c7e2f",
+    "url": "/pokemon-db/static/js/2.49495cde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-db/static/js/2.49495cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e704b2696a133b39891",
+    "url": "/pokemon-db/static/js/main.218542d9.chunk.js"
+  },
+  {
+    "revision": "e3ecc91a4d1cc46464a6",
+    "url": "/pokemon-db/static/js/runtime-main.99f1db4e.js"
+  },
+  {
+    "revision": "135dd48484ddff3539204d5afb6733c5",
+    "url": "/pokemon-db/static/media/picachu-error.135dd484.png"
+  }
+]);
